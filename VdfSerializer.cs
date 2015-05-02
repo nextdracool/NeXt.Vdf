acfe61e13a69eff6575dfc4091f8f95f3fab1c3e
Mirco Gericke <mirco-g.lem@web.de>
2015-05-02 11:25:27 +0200

    Initial commit

---
+++ VdfSerializer.cs
@@ -0,0 +1,143 @@
+﻿using System.Text;
+using System.IO;
+using System.Globalization;
+
+namespace NeXt.Vdf
+{
+    /// <summary>
+    /// Class that handles serialization of VdfValue objects into Vdf formatted strings
+    /// </summary>
+    public class VdfSerializer
+    {
+        private delegate void OnStep(string text);
+
+        /// <summary>
+        /// Creates a VdfSerializer object
+        /// </summary>
+        /// <param name="value">the VdfValue to serialize</param>
+        public VdfSerializer(VdfValue value)
+        {
+            root = value;
+        }
+
+        private VdfValue root;
+        private int indentLevel = 0;
+
+        private string EscapeString(string v)
+        {
+            return v.Replace("\\", @"\\").Replace("\t", @"\t").Replace("\n", @"\n").Replace("\"", @"\""");
+        }
+
+        private void WriteString(OnStep step, string text, bool escape)
+        {
+            step("\"");
+            if(escape)
+            {
+                step(EscapeString(text));
+            }
+            else
+            {
+                step(text);
+            }
+            step("\"");
+        }
+
+        private void WriteToken(OnStep step,  string value)
+        {
+            step("[$");
+            step(EscapeString(value));
+            step("]");
+        }
+
+        private void RunSerialization(OnStep onStep, VdfValue current)
+        {
+            if(current.Comments.Count > 0)
+            {
+                foreach(var s in current.Comments)
+                {
+                    onStep("  ".Repeat(indentLevel));
+                    onStep("//");
+                    onStep(EscapeString(s));
+                    onStep("\n");
+                }
+            }
+
+            onStep("  ".Repeat(indentLevel));
+            WriteString(onStep, current.Name, true);
+            switch(current.Type)
+            {
+                case VdfValueType.String:
+                {
+                    onStep(" ");
+                    WriteString(onStep, (current as VdfString).Content, true);
+  
+                    onStep("\n");
+                    break;
+                }
+                case VdfValueType.Integer:
+                {
+                    onStep(" ");
+                    WriteString(onStep, (current as VdfInteger).Content.ToString(CultureInfo.InvariantCulture), false);
+
+                    onStep("\n");
+                    break;
+                }
+                case VdfValueType.Double:
+                {
+                    onStep(" ");
+                    WriteString(onStep, (current as VdfDouble).Content.ToString(CultureInfo.InvariantCulture), false);
+                    onStep("\n");
+                    break;
+                }
+                case VdfValueType.Table:
+                {
+
+                    onStep("\n");
+                    onStep("  ".Repeat(indentLevel));
+                    onStep("{\n");
+                    indentLevel++;
+                    foreach(var v in current as VdfTable)
+                    {
+                        RunSerialization(onStep, v);
+                    }
+                    indentLevel--;
+                    onStep("  ".Repeat(indentLevel));
+                    onStep("}\n");
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Serialize the object to string
+        /// </summary>
+        /// <returns>a string representing the VdfValue</returns>
+        public string Serialize()
+        {
+            StringBuilder sb = new StringBuilder();
+            RunSerialization((s) => sb.Append(s), root);
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Serialize the object to a file
+        /// </summary>
+        /// <param name="filePath">full path to the file to serialize into</param>
+        public void Serialize(string filePath)
+        {
+            Serialize(filePath, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Serialize the object to a file using the given encoding
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="encoding"></param>
+        public void Serialize(string filePath, Encoding encoding)
+        {
+            StreamWriter writer = new StreamWriter(filePath, false, encoding);
+            RunSerialization((s) => writer.Write(s), root);
+        }
+    }
+}
---
+++ VdfTable.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+
+namespace NeXt.Vdf
+{
+    /// <summary>
+    /// A VdfValue that represents a table containing other VdfValues
+    /// </summary>
+    public sealed class VdfTable : VdfValue, IList<VdfValue>
+    {
+        public VdfTable(string name) : base(name)
+        {
+
+        }
+
+        private List<VdfValue> values = new List<VdfValue>();
+        private Dictionary<string, VdfValue> valuelookup = new Dictionary<string,VdfValue>();
+
+        internal VdfValue owner;
+
+        public int IndexOf(VdfValue item)
+        {
+            return values.IndexOf(item);
+        }
+
+        public void Insert(int index, VdfValue item)
+        {
+            item.Parent = this;
+
+            values.Insert(index, item);
+            valuelookup.Add(item.Name, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            var val = values[index];
+            values.RemoveAt(index);
+            valuelookup.Remove(val.Name);
+            
+        }
+
+        public VdfValue this[int index]
+        {
+            get
+            {
+                return values[index];
+            }
+            set
+            {
+                if(values[index].Name != value.Name)
+                {
+                    valuelookup.Remove(values[index].Name);
+                    valuelookup.Add(value.Name, value);
+                }
+                else
+                {
+                valuelookup[value.Name] = value;
+                }
+                values[index] = value;
+            }
+        }
+
+        public VdfValue this[string name]
+        {
+            get
+            {
+                return valuelookup[name];
+            }
+        }
+
+        public void Add(VdfValue item)
+        {
+            item.Parent = this;
+
+            values.Add(item);
+            valuelookup.Add(item.Name, item);
+        }
+
+        public void Clear()
+        {
+            values.Clear();
+            valuelookup.Clear();
+        }
+
+        public bool Contains(VdfValue item)
+        {
+            return valuelookup.ContainsKey(item.Name);
+        }
+
+        public void CopyTo(VdfValue[] array, int arrayIndex)
+        {
+            values.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return values.Count; }
+        }
+
+        public bool Remove(VdfValue item)
+        {
+            valuelookup.Remove(item.Name);
+            return values.Remove(item);
+        }
+
+        public IEnumerator<VdfValue> GetEnumerator()
+        {
+            return values.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return values.GetEnumerator();
+        }
+
+        bool ICollection<VdfValue>.IsReadOnly
+        {
+            get { return false; }
+        }
+
+    }
+}
